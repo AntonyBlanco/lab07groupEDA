8247484285e5cd00e9c1a9b48a34ee3343b93f44
Edisson Franklin <69181980+Edisson-Acc-UNSA@users.noreply.github.com>
2022-08-14 22:29:18 -0500

    Implementando las clases HasTableClass, Node, test2
    agregamos clases adicionales a la propuesta

--- Ejercicios Propuestos/src/Propuestos/Test2.java
+++ Ejercicios Propuestos/src/Propuestos/Test2.java
@@ -1,5 +1,23 @@
 package Propuestos;
-
 public class Test2 {
-
+	public static void main(String[] args) {
+		System.out.println("Pruebas de Implementación de HashTable");
+		HashTableClass tabla = new HashTableClass(10);
+		System.out.println(tabla.size());
+		String cad ="dos";
+		int prueba= cad.hashCode();
+		System.out.println(prueba%10);
+		System.out.println(tabla.put("uno", 1));
+		System.out.println(tabla.put("dos", 2));
+		System.out.println(tabla.put("tres", 3));
+		System.out.println(tabla.get("uno"));
+		System.out.println(tabla.get("dos"));
+		System.out.println(tabla.size());
+		tabla.clear();
+		System.out.println(tabla.get("uno"));
+		System.out.println(tabla.get("dos"));
+		System.out.println(tabla.size());
+	
+	}
+	
 }
